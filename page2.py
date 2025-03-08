93890264495f919b29db9ec0937085a4cfc14d7f
dennislau25 <dennis.lau@chainofdemand.co>
2025-03-08 04:52:58 +0000

    Chart Updated

--- page2.py
+++ page2.py
@@ -6,12 +6,12 @@ from st_files_connection import FilesConnection
 
 conn = st.connection('s3', type=FilesConnection)
 df = conn.read("hylau-personal-dashboard-data/Lapse_Data.csv", input_format="csv", ttl=600)
-df.sort_index(ascending=False, inplace=True)
+df.set_index('Date', inplace=True)
+df.sort_index(ascending=True, inplace=True)
 
 df['Minutes_Avg_Month'] = df['Minutes'].rolling(window=30).mean()
 df['Minutes_Avg_CMA'] = df['Minutes'].expanding().mean()
 overall_mean = df['Minutes'].mean()
 
-st.line_chart(df, x="Date", y="Minutes_Avg_Month")
-st.line_chart(df, x="Date", y="Minutes_Avg_CMA")
+st.line_chart(df[['Minutes_Avg_Month', 'Minutes_Avg_CMA']])
 st.write(f'AVERAGE: {overall_mean}')
