5b8dc998ee5b577050f7d082ea1499856e5576df
dennislau25 <dennis.lau@chainofdemand.co>
2025-07-24 16:38:17 +0100

    Graphs Updated

--- page2.py
+++ page2.py
@@ -14,12 +14,13 @@ df1['Minutes_Avg_Year'] = df1['Minutes'].rolling(window=365).mean()
 df1['Minutes_Avg_CMA'] = df1['Minutes'].expanding().mean()
 overall_mean = df1['Minutes'].mean()
 
-df1['Lapse_Avg_Month'] = df1['Lure Lapse'].rolling(window=30).mean()
-df1['Lapse_Avg_Year'] = df1['Lure Lapse'].rolling(window=365).mean()
-annual_lapse_mean = df1['Lure Lapse'].tail(365).mean()
+df1['Lapse_Avg_Month'] = df1[df1.index >= '2024-05-27 00:00:00']['Lure Lapse'].rolling(window=30).mean()
+df1['Lapse_Avg_Year'] = df1[df1.index >= '2024-05-27 00:00:00']['Lure Lapse'].rolling(window=365).mean()
+df1['Lapse_Avg_CMA'] = df1[df1.index >= '2024-05-27 00:00:00']['Lure Lapse'].expanding().mean()
+overall_lapse_mean = df1[df1.index >= '2024-05-27 00:00:00']['Lure Lapse'].mean()
 
 st.write(f'AVERAGE: {overall_mean:.2f}')
 st.line_chart(df1[['Minutes_Avg_Month', 'Minutes_Avg_Year', 'Minutes_Avg_CMA']].tail(365*5))
 
-st.write(f'ANNUAL AVERAGE: {annual_lapse_mean:.3f}')
-st.line_chart(df1[['Lapse_Avg_Month', 'Lapse_Avg_Year']].tail(365*1))
+st.write(f'AVERAGE: {overall_lapse_mean:.3f}')
+st.line_chart(df1[df1.index >= '2024-06-15 00:00:00'][['Lapse_Avg_Month', 'Lapse_Avg_Year', 'Lapse_Avg_CMA']].tail(365*5))
--- page4.py
+++ page4.py
@@ -10,8 +10,17 @@ df2.set_index('Date', inplace=True)
 df2.sort_index(ascending=True, inplace=True)
 
 df2['Calories_Avg_Month'] = df2['Calories_Intake'].rolling(window=30).mean()
+df2['Calories_Avg_Year'] = df2['Calories_Intake'].rolling(window=365).mean()
 df2['Calories_Avg_CMA'] = df2['Calories_Intake'].expanding().mean()
 overall_mean = df2['Calories_Intake'].mean()
 
+df2['Cans_Avg_Month'] = df2[df2.index >= '2025-06-10 00:00:00']['Cans'].rolling(window=30).mean()
+df2['Cans_Avg_Year'] = df2[df2.index >= '2025-06-10 00:00:00']['Cans'].rolling(window=365).mean()
+df2['Cans_Avg_CMA'] = df2[df2.index >= '2025-06-10 00:00:00']['Cans'].expanding().mean()
+cans_overall_mean = df2[df2.index >= '2025-06-10 00:00:00']['Cans'].mean()
+
 st.write(f'AVERAGE: {overall_mean:.0f}')
-st.line_chart(df2[['Calories_Avg_Month', 'Calories_Avg_CMA']])
+st.line_chart(df2[['Calories_Avg_Month', 'Calories_Avg_Year', 'Calories_Avg_CMA']].tail(365*5))
+
+st.write(f'AVERAGE: {cans_overall_mean:.3f}')
+st.line_chart(df2[df2.index >= '2025-06-10 00:00:00'][['Cans_Avg_Month', 'Cans_Avg_Year', 'Cans_Avg_CMA']].tail(365*5))
